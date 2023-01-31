6c7964dae806e74b91c0208ce9ee30880ef55326
Centurion195 <Vadim-0211@yandex.ru>
2023-01-31 18:40:12 +0300

    Task #1 completed

---
+++ #1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите номер дня недели: ");
+char day = Convert.ToChar(Console.ReadLine());
+if (day<'8')
+{
+    if (day=='6' || day=='7')
+    Console.WriteLine("Этот день выходной.");
+    else
+    Console.WriteLine("Этот день не выходной.");
+}
+else
+Console.WriteLine("Такого дня недели нет!");
